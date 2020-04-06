ae6439856307cb5123a62a675161844bee267206
DavidWidjaja-137 <david.widjaja@alumni.ubc.ca>
2020-04-05 19:38:26 -0700

    Improved system of evaluating turns to become more reliable. Did a bit of code cleanup

--- scripts/driver.py
+++ scripts/driver.py
@@ -9,6 +9,11 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from geometry_msgs.msg import Twist
 
+#Robot Camera Parameters
+IMG_WIDTH = 640
+#Image Height has to be less than 480
+IMG_HEIGHT = 100
+
 #Only road lines are white, other features are black
 BINARY_THRESH = 250
 
@@ -17,19 +22,21 @@ CANNY_H = 200
 CANNY_L = 25
 
 #Probabilistic Hough Transform parameters
-HOUGH_THRESH = 75
-MIN_LINE = 20
+#100 gives too little lines
+#85 is okay-ish
+#75 gives too many lines
+#30 gives too many lines
+HOUGH_THRESH_P = 85
+MIN_LINE = 25
 MAX_GAP = 75
 
 #Direction Thesholds
-#Forward Threshold must be high >=3 so that turns are executed completely.
-FWD_THRESH = 3
-LEFT_THRESH = 1
-RIGHT_THRESH = 1
-#FWD_THRESH = 2
-#LEFT_THRESH = 2
-#RIGHT_THRESH = 2
-MID_THRESH = 1
+#If the forward thresholds are too high, false negatives will occur
+#If the forward thresholds are too low, false positives will occur
+FWD_THRESH_P = 3        #at least 3 forward-facing lines should be available
+LEFT_THRESH_P = 1
+RIGHT_THRESH_P = 1
+MID_THRESH_P = 1
 
 #Codes
 IDK = -1
@@ -45,15 +52,39 @@ integral_response = 0
 
 #Misc Global Variables
 lock_movement = False
+evaluation_lock = -1
+fwd_total = 0
+idk_total = 0
+mid_total = 0
 movement = FWD
 move = Twist()
 n_turns = 0
 
-def edge_detector(cv_img_binary, blank_image, frame_width, draw = True):
+def edge_detector(cv_img_binary, blank_image, draw = True):
+
+    #The Hough Transform cannot detect the edges of the lines, making it
+    #   more difficult to use than the Probabilistic Hough Transform
+
+    #Split the image into left and right sections. Horizontally invert the right image
+    
+    #Apply Hough Transform on both the left and inverted-right images
+
+    #lines on the left with angle < pi/3 are left-vertical
+    #lines on the left with angle > pi/3 are left-horizontal
+    #lines on the right with angle < pi/3 are right-vertical
+    #lines on the right with angle > pi/3 are right-horizontal
+
+    #Use Non-Probabilistic Hough Transform to find edges
+    edges = cv.Canny(cv_img_binary, CANNY_L, CANNY_H, apertureSize = 3)
+    lines = cv.HoughLines(edges, 1, np.pi/180, HOUGH_THRESH)
+    
+     
+
+def edge_detector_p(cv_img_binary, blank_image, draw = True):
 
     #Use Probabilistic Hough Transform to find edges
     edges = cv.Canny(cv_img_binary, CANNY_L, CANNY_H, apertureSize = 3)
-    lines = cv.HoughLinesP(edges, 1, np.pi/180, HOUGH_THRESH,\
+    lines = cv.HoughLinesP(edges, 1, np.pi/180, HOUGH_THRESH_P,\
                              minLineLength=MIN_LINE,maxLineGap=MAX_GAP)
     
     line_coords = []
@@ -77,7 +108,7 @@ def edge_detector(cv_img_binary, blank_image, frame_width, draw = True):
             gradient = 1 #definitely vertical
 
         gradients.append(gradient)
-        if gradient > 0.5:
+        if gradient > 0.7:
             if draw == True:
                 cv.line(blank_image, (x1,y1), (x2,y2),\
                          (random.randint(0,255),255,random.randint(0,255)),2)
@@ -87,10 +118,10 @@ def edge_detector(cv_img_binary, blank_image, frame_width, draw = True):
                 cv.line(blank_image, (x1,y1), (x2,y2), (255,0,0),2)
             
             #if x1 or x2 is on the left half, turn is on the left
-            if max(x1,x2) < (frame_width)/2:
+            if max(x1,x2) < (IMG_WIDTH)/2:
                 left_pt = left_pt + 1
             #if x1 or x2 is on the right half, turn is on the right
-            elif min(x1,x2) > (frame_width)/2:
+            elif min(x1,x2) > (IMG_WIDTH)/2:
                 right_pt = right_pt + 1
             #if one coordinate is on the left and another on the right
             else:
@@ -100,7 +131,7 @@ def edge_detector(cv_img_binary, blank_image, frame_width, draw = True):
                 cv.line(blank_image, (x1,y1), (x2,y2), (0,0,255),2)
             idk_pt = idk_pt + 1
 
-    #print("f:{},l:{},r:{},m:{},idk:{}".format(fwd_pt,left_pt,right_pt,middle_pt,idk_pt))
+    print("f:{},l:{},r:{},m:{},idk:{}".format(fwd_pt,left_pt,right_pt,middle_pt,idk_pt))
 
     return ((fwd_pt, left_pt, right_pt, middle_pt, idk_pt), blank_image) 
 
@@ -110,13 +141,13 @@ def determine_motion(edge_points):
     forward = False  
     left = False
     right = False
-    if edge_points[0] >= FWD_THRESH:
+    if edge_points[0] >= FWD_THRESH_P:
         forward = True
-    if edge_points[1] >= LEFT_THRESH:
+    if edge_points[1] >= LEFT_THRESH_P:
         left = True    
-    if edge_points[2] >= RIGHT_THRESH:
+    if edge_points[2] >= RIGHT_THRESH_P:
         right = True
-    if edge_points[3] >= MID_THRESH:
+    if edge_points[3] >= MID_THRESH_P:
         forward = False
      
     #   Forward     Left        Right
@@ -134,27 +165,34 @@ def determine_motion(edge_points):
 def driver(data):
 
     global lock_movement
+    global evaluation_lock
+    global fwd_total
+    global mid_total
+    global idk_total
     global movement
     global n_turns
     global move
 
-    #Apply filters to raw image
+    #Obtain and crop the raw image
     cv_img = bridge.imgmsg_to_cv2(data, "bgr8")
-    cv_img = cv_img[-100:, :, :]
+    cv_img = cv_img[-IMG_HEIGHT:, :, :]
+
+    #Apply greyscale, averaging, and thresholding to get binary image
     cv_img_gray = cv.cvtColor(cv_img, cv.COLOR_BGR2GRAY)
     kernel = np.ones((5,5), np.float32)/25
     cv_img_gray = cv.filter2D(cv_img_gray,-1,kernel)
     retval, cv_img_binary = cv.threshold(cv_img_gray, BINARY_THRESH, 255, cv.THRESH_BINARY)
 
+    #Apply bandpass filter to obtain only red part of image
     cv_redmask = cv.inRange(cv_img, np.array([0,0,200]), np.array([0,0,255]))
 
-    frame_height, frame_width = cv_img_gray.shape
-    blank_image = np.zeros((frame_height, frame_width, 3), np.uint8)
+    #Instantiate a blank array for illustration purposes
+    blank_image = np.zeros((IMG_HEIGHT, IMG_WIDTH, 3), np.uint8)
     
     #Obtain edges to determine current position on board
-    edge_points, blank_image = edge_detector(cv_img_binary, blank_image, frame_width)
+    edge_points, blank_image = edge_detector_p(cv_img_binary, blank_image)
 
-    #Obtain possible movements
+    #Obtain possible movements from the edges
     forward, left, right = determine_motion(edge_points)
     
     #if the vehicle is not in the middle of a turn, determine new movement
@@ -186,64 +224,82 @@ def driver(data):
         elif forward == False:
             # Stop the car. Mission failed...for now
             movement = IDK
-            print("ERROR: Unknown Position.")
+            print("Unknown Position.")
             print("Forward: {} Left: {} Right: {}".format(forward,left,right))
 
     #if the vehicle is in the middle of a turn 
     else:
        
         #if the vehicle has completed a turn, unlock movement 
-        if forward == True and left == False and right == False:
+        if (forward == True and left == False and right == False) or evaluation_lock != -1:
 
+            #Note: The Probabilistic Hough Transform actually produces self-consistent 
+            #   results, as long as the same image is being used. This means that most of the
+            #   error comes from the source image. So to average out random errors, different
+            #   images from the same position must be used.
+        
+            #if the vehicle is not evaluating a turn yet, begin evaluating the turn
+            if evaluation_lock == -1:
 
-            #As the Hough Transform is probabilistic, repeat the Hough Transform 5x
-            # to average out the random errors.
-            
-            move.linear.x = 0.0
-            move.linear.y = 0.0
-            move.angular.z = move.angular.z * 0.5
-            pub_vel.publish(move)
+                print("Evaluation unlocked. Begin evaluating turn {}".format(n_turns))
+                evaluation_lock = 0
+                fwd_total = 0
+                idk_total = 0
+                mid_total = 0
 
-            fwd_total = 0
-            idk_total = 0
-            mid_total = 0
-            for i in range(5):
+            #Take like 5 different images to be sure.
+            elif evaluation_lock < 5:
 
-                edge_points, retval = edge_detector(cv_img_binary, blank_image,\
-                                                     frame_width, draw = False)
-                fwd_total = fwd_total + edge_points[0] 
+                print("Evaluating Turn, Trial {}".format(evaluation_lock))
+                edge_points, retval = edge_detector_p(cv_img_binary, blank_image, draw=False)
+                fwd_total = fwd_total + edge_points[0]
                 mid_total = mid_total + edge_points[3]
                 idk_total = idk_total + edge_points[4]
 
-            redmask_sum = cv_redmask.sum()
-            
-            
-            if fwd_total >= (20) and idk_total == 0 and mid_total == 0 and redmask_sum < 500000:
-                print("Turn {} Complete".format(n_turns))
-                print("fwd_total: {}, redmask_sum: {}".format(fwd_total, redmask_sum))
-                cv.imshow("turn_completed_{}_edges".format(n_turns), blank_image)
-                cv.imshow("turn_completed_{}_color".format(n_turns), cv_img)
-                movement = FWD
-                lock_movement = False
-                n_turns = n_turns + 1
-            else:
-                print("random: fwd: {}, red: {}, idk: {} mid: {}".format(\
-                        fwd_total, redmask_sum, idk_total, mid_total))
+                evaluation_lock = evaluation_lock + 1
+ 
+            elif evaluation_lock == 5:
 
+                redmask_sum = cv_redmask.sum()
+            
+                print("Evaluation Data for turn {} obtained.".format(n_turns))
+            
+                #evaluate whether the turn is really complete with stricter conditions 
+                if fwd_total >= (20) and idk_total == 0 and mid_total == 0 \
+                     and redmask_sum < 500000:
+            
+                    print("Turn {} Complete".format(n_turns))
+                    print("fwd_total: {}, redmask_sum: {}".format(fwd_total, redmask_sum))
+                    cv.imshow("turn_completed_{}_edges".format(n_turns), blank_image)
+                    cv.imshow("turn_completed_{}_color".format(n_turns), cv_img)
+                    movement = FWD
+                    lock_movement = False
+                    n_turns = n_turns + 1
+
+                    #TEMPORARY: DELAY 
+                    time.sleep(10)
+
+                else:
+                    print("Turn Incomplete: fwd: {}, red: {}, idk: {} mid: {}".format(\
+                    fwd_total, redmask_sum, idk_total, mid_total))
+
+                #release the evaluation lock, allowing the car to continue turning
+                evaluation_lock = -1
+
+    #If the vehicle should move forward, apply PID on the edges of the line
     if movement == FWD:
-
+        
         #sum the thresholded image to reduce random error
         row = []
-        for i in range(frame_width):
+        for i in range(IMG_WIDTH):
             vert = 0
-            for j in range(frame_height):
+            for j in range(IMG_HEIGHT):
                 vert = vert + cv_img_binary[j][i]
-            
             row.append(vert)
         
         #find the lower bound
-        lower = frame_width/2
-        c = frame_width/2
+        lower = IMG_WIDTH/2
+        c = IMG_WIDTH/2
         lower_found = False
         while(c > -1):
             
@@ -257,10 +313,10 @@ def driver(data):
             lower = 0
         
         #find the upper bound
-        upper = frame_width/2
-        c = frame_width/2
+        upper = IMG_WIDTH/2
+        c = IMG_WIDTH/2
         upper_found = False
-        while (c < frame_width - 1):
+        while (c < IMG_WIDTH - 1):
             
             if row[c] > 10*255:
                 upper = c
@@ -269,26 +325,21 @@ def driver(data):
             c = c + 1
         
         if upper_found == False:
-            upper = frame_width - 2
+            upper = IMG_WIDTH - 2
  
-        cv.circle(blank_image, (upper, frame_height - 10), 10, (0,0,255), -1)
-        cv.circle(blank_image, (lower, frame_height - 10), 10, (0,255,0), -1)
+        cv.circle(blank_image, (upper, IMG_HEIGHT - 10), 10, (0,0,255), -1)
+        cv.circle(blank_image, (lower, IMG_HEIGHT - 10), 10, (0,255,0), -1)
         
-        error = frame_width/2 -  int((upper + lower)/2)
+        error = IMG_WIDTH/2 -  int((upper + lower)/2)
         
-        #print("Upper: {} Lower: {} Err: {}".format(upper, lower, error))
-        
-        #Do PID
         move = Twist()
         move.linear.x = 0.20
         move.linear.y = 0.0
             
-        kp = 3.0/frame_width
+        kp = 3.0/IMG_WIDTH
         ki = 0
-        kd = 10.0/frame_width
+        kd = 10.0/IMG_WIDTH
 
-        #if error > 0, proportional_response > 0
-        #if error < 0, proportional_response < 0
         proportional_response = kp * error
         global integral_response
         global previous_error
@@ -304,9 +355,9 @@ def driver(data):
         pub_vel.publish(move)
             
         previous_error = error
-        
+
+    #Execute semi-hardcoded subroutine to turn left.        
     elif movement == LEFT:
-        #Do a preprogrammed sharp left turn
 
         #If it is the first movement to the left,
         if lock_movement == False:
@@ -323,7 +374,6 @@ def driver(data):
             move.linear.x = 0.0
             move.linear.y = 0.0
             move.angular.z = 0.0
-
             pub_vel.publish(move)
 
             print("Junction Entered. Beginning LEFT TURN")
@@ -332,18 +382,27 @@ def driver(data):
             lock_movement = True
 
         #If the vehicle is in the middle of a turn
-        else:
+        elif evaluation_lock == -1:
 
             #Send appropriate turning messages
             move = Twist()
             move.linear.x = 0.0
             move.linear.y = 0.0
             move.angular.z = 0.35
-
             pub_vel.publish(move)
-         
+        
+        #If the vehicle is evaluating a turn
+        else:
+ 
+            #Stop the car
+            move = Twist()
+            move.linear.x = 0.0
+            move.linear.y = 0.0
+            move.angular.z = 0.0
+            pub_vel.publish(move)        
+
+    #Execute semi-hardcoded subroutine to turn right         
     elif movement == RIGHT:
-        #Do a preprogammed sharp right turn
 
         #If it is the first movement to the right,
         if lock_movement == False:
@@ -369,18 +428,27 @@ def driver(data):
             lock_movement = True
 
         #If the vehicle is in the middle of a turn
-        else:
+        elif evaluation_lock == -1:
 
             #Send appropriate turning messages
             move = Twist()
             move.linear.x = 0.0
             move.linear.y = 0.0
             move.angular.z = -0.35
-
             pub_vel.publish(move)
+        
+        #If the vehicle is evaluating a turn
+        else:
 
+            #Stop the car
+            move = Twist()
+            move.linear.x = 0.0
+            move.linear.y = 0.0
+            move.angular.z = 0.0
+            pub_vel.publish(move)        
+
+    #Vehicle is in an unknown position; Stop the vehicle
     elif movement == IDK:
-        #shutdown
 
         move = Twist()
         move.linear.x = 0.0
@@ -390,8 +458,8 @@ def driver(data):
         
         print("Vehicle has been stopped.")
         cv.imshow('error', blank_image)
-        cv.waitKey(1)
-        time.sleep(50)
+        
+        #continue running, as this might just be a random error
 
     cv.imshow('img', blank_image)
     cv.waitKey(1)
@@ -404,7 +472,9 @@ def observer(data):
 
     #Apply filters to raw image
     cv_img = bridge.imgmsg_to_cv2(data, "bgr8")
-    cv_img = cv_img[-100:, :, :]
+
+    cv_img = cv_img[-IMG_HEIGHT:, :, :]
+
     cv_img_gray = cv.cvtColor(cv_img, cv.COLOR_BGR2GRAY)
     kernel = np.ones((5,5), np.float32)/25
     cv_img_gray = cv.filter2D(cv_img_gray,-1,kernel)
@@ -413,21 +483,20 @@ def observer(data):
 
     cv_redmask = cv.inRange(cv_img, np.array([0,0,200]), np.array([0,0,255]))
 
-    frame_height, frame_width = cv_img_gray.shape
-    blank_image = np.zeros((frame_height, frame_width, 3), np.uint8)
-    
+    blank_image = np.zeros((IMG_HEIGHT, IMG_WIDTH, 3), np.uint8)
+
     #Obtain edges to determine current position on board
-    edge_points, blank_image = edge_detector(cv_img_binary, blank_image, frame_width)
+    edge_points, blank_image = edge_detector_p(cv_img_binary, blank_image)
 
     #Obtain possible movements
     forward, left, right = determine_motion(edge_points)
 
-    #print("forward: {} left: {} right: {}".format(forward,left,right))
-
-    #print("fwd_pt: {} left_pt: {} right_pt: {} mid_pt: {}".format(\
-    #        edge_points[0], edge_points[1], edge_points[2], edge_points[3]))
+    print("f: {} l: {} r: {} mid: {} fwd: {} left: {} right: {}".format(\
+        edge_points[0], edge_points[1], edge_points[2], edge_points[3], \
+        forward, left, right))
 
     cv.imshow("observer", blank_image)
+    cv.imshow("grayscale", cv_img_gray)
     cv.waitKey(1)
 
 #Initialize the node
@@ -437,9 +506,10 @@ rospy.loginfo('driver node started')
 #Publish a movement topic
 pub_vel = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
 
+#Delay for observation purposes
 time.sleep(10)
 
-#Initialize the movement
+#Initialize the movement with a slight angle to demonstrate PID in action
 move = Twist()
 move.linear.x = 0.35
 move.linear.y = 0.0
@@ -447,7 +517,6 @@ move.linear.z = 0.0
 move.angular.x = 0.0
 move.angular.y = 0.0
 move.angular.z = 0.3
-
 pub_vel.publish(move)
 
 time.sleep(0.3)
@@ -458,16 +527,13 @@ move.linear.z = 0.0
 move.angular.x = 0.0
 move.angular.y = 0.0
 move.angular.z = 0.0
-
 pub_vel.publish(move)
 
 #Subscribe to camera
-sub_image = rospy.Subscriber("/rrbot/camera1/image_raw", Image, observer)
+sub_image = rospy.Subscriber("/rrbot/camera1/image_raw", Image, driver)
 
 #Bridge ros and opencv
 bridge = CvBridge()
 
-rospy.loginfo('robot movement initialized')
-
-#TEMP: keep running
+#Stops driver node from dying
 rospy.spin()
